b9d502b765b96802387cc254a4b73ee73d42ca4f
Shuaib Abdulrazaq <67469541+Zhaqyy@users.noreply.github.com>
2023-02-02 18:20:54 +0100

    -Added a welcome text to hero -Added colored shape beneath to cut empty space -Added a css Reset file

--- src/Body/hero/hero.js
+++ src/Body/hero/hero.js
@@ -7,16 +7,25 @@ function Hero() {
       <div className="welcome">
         <h2>WELCOME !</h2>
         <h1>Shuaib Abdulrazaq</h1>
-        <h2>Here</h2>
+        <h2><span>Here</span></h2>
       </div>
+
       <div className="titles">
         <ul>
           <li>FrontEnd Developer</li>
           <li>Designer</li>
           <li>Professional Clown</li>
         </ul>
-        <a className="download" href="/">Download Resume</a>
+        <p><a className="download" href="/">Download Resume</a></p>
       </div>
+      <div className="Floaty">
+      <p><a href="#about">About</a></p>
+      <span>
+
+      </span>
+      <p><a href="#exp">Experience</a></p>
+      </div>
+      {/* <div className="righty Floaty"></div> */}
     </section>
   );
 }
